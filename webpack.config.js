d4129e9ea381d1f67a579dc7fcb8ae242079ce7c
Austin Jang <austinhjang@gmail.com>
2020-01-14 21:44:00 -0800

    Add frontend file structure and webpack config file

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: path.resolve(__dirname, 'client', 'src', 'index.jsx'),
+  publicPath: '/',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'client', 'public')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          query: {
+            presets: ["@babel/preset-env", "@babel/preset-react"]
+          }
+        }
+      }
+    ]
+  }
+}
