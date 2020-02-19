0ad83320385f697ca26186345f5f9f6eccec2cb6
Austin Jang <austinjang11@gmail.com>
2020-02-18 20:06:34 -0800

    Make fresh angular project

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,12 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TextboxComponent } from './textbox/textbox.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    TextboxComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
