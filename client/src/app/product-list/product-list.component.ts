269cce71aa7178a92e889b980c57a8575b121ccb
Austin Jang <austinjang11@gmail.com>
2020-02-25 00:00:27 -0800

    Remove unused elements, Add product cards and hard-coded keyset data

--- client/src/app/product-list/product-list.component.ts
+++ client/src/app/product-list/product-list.component.ts
@@ -9,6 +9,8 @@ export class ProductListComponent implements OnInit {
 
   constructor() { }
 
+  products = [{name: 'GMK Laser', vendor: 'drop', url: 'https://drop.com/buy/massdrop-x-mito-gmk-laser-custom-keycap-set', image_url: 'https://massdrop-s3.imgix.net/img_thread/BZ27br1TIOyV6GfQvgZg_TKL%208.png?auto=format&fm=jpg&fit=min&w=796&dpr=2&q=35'}, {name: 'GMK Red Samurai', vendor: 'drop', url: 'https://drop.com/buy/massdrop-x-mito-gmk-laser-custom-keycap-set', image_url: 'https://massdrop-s3.imgix.net/img_thread/BZ27br1TIOyV6GfQvgZg_TKL%208.png?auto=format&fm=jpg&fit=min&w=796&dpr=2&q=35'}, {name: 'GMK Godspeed', vendor: 'drop', url: 'https://drop.com/buy/massdrop-x-mito-gmk-laser-custom-keycap-set', image_url: 'https://massdrop-s3.imgix.net/img_thread/BZ27br1TIOyV6GfQvgZg_TKL%208.png?auto=format&fm=jpg&fit=min&w=796&dpr=2&q=35'}, {name: 'GMK Nautilus', vendor: 'drop', url: 'https://drop.com/buy/massdrop-x-mito-gmk-laser-custom-keycap-set', image_url: 'https://massdrop-s3.imgix.net/img_thread/BZ27br1TIOyV6GfQvgZg_TKL%208.png?auto=format&fm=jpg&fit=min&w=796&dpr=2&q=35'}, {name: 'GMK Olivia', vendor: 'novelkeys', url: 'https://drop.com/buy/massdrop-x-mito-gmk-laser-custom-keycap-set', image_url: 'https://massdrop-s3.imgix.net/img_thread/BZ27br1TIOyV6GfQvgZg_TKL%208.png?auto=format&fm=jpg&fit=min&w=796&dpr=2&q=35'}];
+
   ngOnInit() {
   }
 
