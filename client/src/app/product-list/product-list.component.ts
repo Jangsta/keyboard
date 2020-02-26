4145e4a7c6809662301c5ae228c0fe5a85e32af2
Austin Jang <austinjang11@gmail.com>
2020-02-25 23:59:50 -0800

    Add apollo and basic keyset graphql query, Add cors middleware to express server

--- client/src/app/product-list/product-list.component.ts
+++ client/src/app/product-list/product-list.component.ts
@@ -1,4 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { Apollo } from 'apollo-angular';
+import gql from 'graphql-tag';
+
+type Keyset = {
+  name: String,
+  id: String,
+  manufacturer: String,
+  keyset_id: String,
+  material: String,
+  profile: String,
+  kits: String,
+  tags: String,
+  vendor: String[]
+  url: String,
+  image_url: String,
+  description: String,
+  price: Number,
+  quantity: Number,
+  available: Boolean
+};
+
+type Response = {
+  keysets: Keyset[]
+}
 
 @Component({
   selector: 'app-product-list',
@@ -7,11 +31,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductListComponent implements OnInit {
 
-  constructor() { }
-
-  products = [{name: 'GMK Laser', vendor: 'drop', url: 'https://drop.com/buy/massdrop-x-mito-gmk-laser-custom-keycap-set', image_url: 'https://massdrop-s3.imgix.net/img_thread/BZ27br1TIOyV6GfQvgZg_TKL%208.png?auto=format&fm=jpg&fit=min&w=796&dpr=2&q=35'}, {name: 'GMK Red Samurai', vendor: 'drop', url: 'https://drop.com/buy/massdrop-x-mito-gmk-laser-custom-keycap-set', image_url: 'https://massdrop-s3.imgix.net/img_thread/BZ27br1TIOyV6GfQvgZg_TKL%208.png?auto=format&fm=jpg&fit=min&w=796&dpr=2&q=35'}, {name: 'GMK Godspeed', vendor: 'drop', url: 'https://drop.com/buy/massdrop-x-mito-gmk-laser-custom-keycap-set', image_url: 'https://massdrop-s3.imgix.net/img_thread/BZ27br1TIOyV6GfQvgZg_TKL%208.png?auto=format&fm=jpg&fit=min&w=796&dpr=2&q=35'}, {name: 'GMK Nautilus', vendor: 'drop', url: 'https://drop.com/buy/massdrop-x-mito-gmk-laser-custom-keycap-set', image_url: 'https://massdrop-s3.imgix.net/img_thread/BZ27br1TIOyV6GfQvgZg_TKL%208.png?auto=format&fm=jpg&fit=min&w=796&dpr=2&q=35'}, {name: 'GMK Olivia', vendor: 'novelkeys', url: 'https://drop.com/buy/massdrop-x-mito-gmk-laser-custom-keycap-set', image_url: 'https://massdrop-s3.imgix.net/img_thread/BZ27br1TIOyV6GfQvgZg_TKL%208.png?auto=format&fm=jpg&fit=min&w=796&dpr=2&q=35'}];
-
+  keysets: any[];
+  loading = true;
+  error: any;
+  products: Object[] = [{name: 'GMK Laser', vendor: 'drop', url: 'https://drop.com/buy/massdrop-x-mito-gmk-laser-custom-keycap-set', image_url: 'https://massdrop-s3.imgix.net/img_thread/BZ27br1TIOyV6GfQvgZg_TKL%208.png?auto=format&fm=jpg&fit=min&w=796&dpr=2&q=35'}, {name: 'GMK Red Samurai', vendor: 'drop', url: 'https://drop.com/buy/massdrop-x-mito-gmk-laser-custom-keycap-set', image_url: 'https://massdrop-s3.imgix.net/img_thread/BZ27br1TIOyV6GfQvgZg_TKL%208.png?auto=format&fm=jpg&fit=min&w=796&dpr=2&q=35'}, {name: 'GMK Godspeed', vendor: 'drop', url: 'https://drop.com/buy/massdrop-x-mito-gmk-laser-custom-keycap-set', image_url: 'https://massdrop-s3.imgix.net/img_thread/BZ27br1TIOyV6GfQvgZg_TKL%208.png?auto=format&fm=jpg&fit=min&w=796&dpr=2&q=35'}, {name: 'GMK Nautilus', vendor: 'drop', url: 'https://drop.com/buy/massdrop-x-mito-gmk-laser-custom-keycap-set', image_url: 'https://massdrop-s3.imgix.net/img_thread/BZ27br1TIOyV6GfQvgZg_TKL%208.png?auto=format&fm=jpg&fit=min&w=796&dpr=2&q=35'}, {name: 'GMK Olivia', vendor: 'novelkeys', url: 'https://drop.com/buy/massdrop-x-mito-gmk-laser-custom-keycap-set', image_url: 'https://massdrop-s3.imgix.net/img_thread/BZ27br1TIOyV6GfQvgZg_TKL%208.png?auto=format&fm=jpg&fit=min&w=796&dpr=2&q=35'}];
+  
+  constructor(private apollo: Apollo) { }
+  
   ngOnInit() {
+    this.apollo.watchQuery<Response>({
+      query: gql`
+        {
+          keysets {
+            name
+            vendor {
+              name
+            }
+          }
+        }`
+    }).valueChanges.subscribe((result) => {
+      console.log(result);
+      this.products = result.data && result.data.keysets;
+      this.loading = result.loading;
+      this.error = result.errors;
+    });
   }
 
 }
--- server/index.js
+++ server/index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-
+const cors = require('cors');
 // const router = require('./routes/router.js');
 // const controller = require('./controllers/index.js');
 
 const graphqlHTTP = require('express-graphql');
 const schema = require('./db/schemas/graphqlschema.js');
+
+app.use(cors());
 
 app.use('/graphql', graphqlHTTP({
   schema: schema,
