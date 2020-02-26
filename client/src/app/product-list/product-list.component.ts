f5260b6eadba4878ab2a5d834d3e408ea579e9fe
Austin Jang <austinjang11@gmail.com>
2020-02-26 00:35:04 -0800

    Remove hard-coded data, Minor fix to keyset properties displayed

--- client/src/app/product-list/product-list.component.ts
+++ client/src/app/product-list/product-list.component.ts
@@ -34,7 +34,7 @@ export class ProductListComponent implements OnInit {
   keysets: any[];
   loading = true;
   error: any;
-  products: Object[] = [{name: 'GMK Laser', vendor: 'drop', url: 'https://drop.com/buy/massdrop-x-mito-gmk-laser-custom-keycap-set', image_url: 'https://massdrop-s3.imgix.net/img_thread/BZ27br1TIOyV6GfQvgZg_TKL%208.png?auto=format&fm=jpg&fit=min&w=796&dpr=2&q=35'}, {name: 'GMK Red Samurai', vendor: 'drop', url: 'https://drop.com/buy/massdrop-x-mito-gmk-laser-custom-keycap-set', image_url: 'https://massdrop-s3.imgix.net/img_thread/BZ27br1TIOyV6GfQvgZg_TKL%208.png?auto=format&fm=jpg&fit=min&w=796&dpr=2&q=35'}, {name: 'GMK Godspeed', vendor: 'drop', url: 'https://drop.com/buy/massdrop-x-mito-gmk-laser-custom-keycap-set', image_url: 'https://massdrop-s3.imgix.net/img_thread/BZ27br1TIOyV6GfQvgZg_TKL%208.png?auto=format&fm=jpg&fit=min&w=796&dpr=2&q=35'}, {name: 'GMK Nautilus', vendor: 'drop', url: 'https://drop.com/buy/massdrop-x-mito-gmk-laser-custom-keycap-set', image_url: 'https://massdrop-s3.imgix.net/img_thread/BZ27br1TIOyV6GfQvgZg_TKL%208.png?auto=format&fm=jpg&fit=min&w=796&dpr=2&q=35'}, {name: 'GMK Olivia', vendor: 'novelkeys', url: 'https://drop.com/buy/massdrop-x-mito-gmk-laser-custom-keycap-set', image_url: 'https://massdrop-s3.imgix.net/img_thread/BZ27br1TIOyV6GfQvgZg_TKL%208.png?auto=format&fm=jpg&fit=min&w=796&dpr=2&q=35'}];
+  products: Object[];
   
   constructor(private apollo: Apollo) { }
   
@@ -44,6 +44,8 @@ export class ProductListComponent implements OnInit {
         {
           keysets {
             name
+            url
+            image_url
             vendor {
               name
             }
